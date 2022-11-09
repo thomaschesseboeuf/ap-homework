9ef3a4b2a146114114838c604f61517323d6890f
Thomas Chesseboeuf <thomas.chesseboeuf@etu.minesparis.psl.eu>
2022-11-09 19:23:14 +0100

    ajout du argparse

--- files-scanext/scan.py
+++ files-scanext/scan.py
@@ -1,7 +1,9 @@
-def scan(str, extent):
+def scan(filename, extent):
     from datetime import datetime as dt
     from pathlib import Path
-    p = Path(str)
+    p = Path(filename)
+    if not (p.is_dir()):
+        print('error, filename does not exist')
     TREE = list(p.glob("**/*" + extent))
     if len(TREE) == 0:
         print('no such file') 
@@ -12,3 +14,12 @@ def scan(str, extent):
         with open(file) as f:
             line = f.readline()
         print(f'first line : {line}')
+        
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', help = 'indiquer le chemin du dossier sous forme de filename')
+parser.add_argument('extent', help = 'indiquer l extension sous forme de filename')
+args = parser.parse_args()
+filename = args.filename
+extent = args.extent
+scan(filename, extent)
