bee4068897cc0f95e90c20624e371c18c7ad68b7
Thomas Chesseboeuf <thomas.chesseboeuf@etu.minesparis.psl.eu>
2023-01-19 12:19:09 +0100

    v11

--- boids/boids-01.py
+++ boids/boids-01.py
@@ -8,6 +8,19 @@ OBSTACLE = 'obstacle-resized.png'
 HEIGHT = 800
 WIDTH = 800
 
+def grid():
+    Grid = []
+    X = np.linspace(0,WIDTH,12)
+    Y = np.linspace(0,HEIGHT,12)
+    for x in X[1:-1]:
+        for y in Y[1:-1]:
+            Grid.append((x,y))
+    return Grid
+
+def distance(a, b):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**0.5
+
+
 class Window(arcade.Window):
 
     def __init__(self):
@@ -40,11 +53,8 @@ class Window(arcade.Window):
         self.boids = arcade.SpriteList()
         self.boids.append(boid)
         self.obstacles = arcade.SpriteList()
-        X = np.linspace(0,WIDTH,12)
-        Y = np.linspace(0,HEIGHT,12)
-        for x in X[1:-1]:
-            for y in Y[1:-1]:
-                self.obstacles.append(Obstacle(x,y))
+        for (x,y)in grid():
+            self.obstacles.append(Obstacle(x,y))
 
     def on_draw(self):
         arcade.start_render()
@@ -59,20 +69,42 @@ class Boid(arcade.Sprite):
     def __init__(self):
         super().__init__(IMAGE)
         self.turning = 0
+        self.alpha = 255
+        self.obstacles = arcade.SpriteList()
+        for (x,y) in grid():
+            self.obstacles.append(Obstacle(x,y))
+        self.distances = []
+        for obstacle in self.obstacles:
+            self.distances.append(distance((self.center_x, self.center_y), (obstacle.center_x, obstacle.center_y)))
         self.rotation_speed = 100
         self.angle = -135
         self.speed = 100
         self.center_x, self.center_y = 100, 100
+
     
     def on_update(self, delta_time):
         self.angle += np.random.normal(0, 1) + self.turning * self.rotation_speed * delta_time
         self.center_x += self.speed * math.cos(self.angle*math.pi/180) * delta_time
-        self.center_x = self.center_x % 800
         self.center_y += self.speed * math.sin(self.angle*math.pi/180)  * delta_time
+        self.distances = []
+        for obstacle in self.obstacles:
+            self.distances.append(distance((self.center_x, self.center_y), (obstacle.center_x, obstacle.center_y)))
+        
+        #SEPARATION
+        delta = [0,0]
+        for i,d in enumerate(self.distances):
+            if d<20:
+                delta[0] += (self.center_x-self.obstacles[i].center_x)*2*(1-d/20)
+                delta[1] += (self.center_y-self.obstacles[i].center_y)*2*(1-d/20)
+        self.center_x += delta[0]
+        self.center_y += delta[1]
+        if delta != [0,0]:
+            self.angle = math.atan2(delta[1], delta[0])
+        
+        self.center_x = self.center_x % 800
         self.center_y = self.center_y % 800
 
 class Obstacle(arcade.Sprite):
-
     def __init__(self, x=WIDTH/2, y=HEIGHT/2):
         super().__init__(OBSTACLE)
         self.center_x, self.center_y = x, y
